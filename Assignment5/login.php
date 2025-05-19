e62d38843fab93b6cc14f664600e94a3e60fa35c
Khalekar Pooja Vikas <khalekarpooja16@gmail.com>
2025-05-19 10:13:36 +0530

    Add files via upload

---
+++ Assignment5/login.php
@@ -0,0 +1,12 @@
+<?php
+$student_id =$_POST['student_id'];
+    echo"Hello Everyone".$student_id."to login.php";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $dob = $_POST['dob'];
+    $gender = $_POST['gender'];
+    $address = $_POST['address'];
+    $program = $_PoST['program'];
+    $academic_background = $_POST['academic_background'];
+    ?>
